ecf418a0b62d3a23361e4a21282875c8308de13e
rfedmi <Dav1dmp>
2015-02-21 17:37:38 -0500

    Primer campio en el proyecto de python en Itsae

---
+++ davidmp/ejemplo.py
@@ -0,0 +1,8 @@
+'''
+Created on 21/2/2015
+
+@author: PC06
+'''
+
+if __name__ == '__main__':
+    pass
--- davidmp/run.py
+++ davidmp/run.py
@@ -2,6 +2,11 @@
 Created on 18/2/2015
 
 @author: PC06
+
+
+David Mamani Pari
+Primer Proyecto de Python con Github
+
 '''
 from app import app
 
